6da3ffa86c40355d52833ddd39908e8a4aceb69a
Harsh-Gehlot <81151468+Harsh-Gehlot@users.noreply.github.com>
2024-04-28 21:11:21 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,4 +3,12 @@ import { } from "../controllers/auth.js";
 
 const router = express.Router();
 
+//CREATE A USER 
+router.post("/signup", )
+//SIGN IN USER
+router.post("/signin", )
+
+//GOOGLE AUTH
+router.post("/google", )
+
 export default router;
